102909089825308588371a67bc6feb0ef750c21a
David <dfs23@njit.edu>
2019-10-25 22:48:43 -0400

    feat: added css file

--- registrationform.php
+++ registrationform.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-include("registrationform.html");
+//include("registrationform.html");
 
 $firstname = filter_input(INPUT_POST, 'firstname');
 $lastname = filter_input(INPUT_POST, 'lastname');
@@ -55,3 +55,41 @@ print "<br><br>";
 print "password: $password";
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Question Form </title>
+    <link rel = "stylesheet" type = "text/css" href = "question.css">
+</head>
+
+<body>
+<main>
+    <h1> Question Form </h1>
+    <form action = "registrationform.php" method = "post">
+
+        <div id = "data ">
+            <label> First Name: </label>
+            <input type = "text" name = "firstname"><br>
+
+            <label> Last Name: </label>
+            <input type = "text" name = "lastname"><br>
+
+            <label> Birthday: </label>
+            <input type="text" name="birthday"><br>
+
+            <label> Email: </label>
+            <input type="text" name="email"><br>
+
+            <label> Password: </label>
+            <input type="text" name="password"><br>
+        </div>
+
+        <div id="buttons">
+            <input type="submit" value = "Submit Responses"><br>
+        </div>
+
+    </form>
+</main>
+</body>
+</html>
